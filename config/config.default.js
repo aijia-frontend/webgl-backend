dbef9b1a6065e53e3f15d2ec734b543feb4c6b4b
caiyong <18651600452@163.com>
2020-03-10 17:45:08 +0800

    配置环境变量

--- config/config.default.js
+++ config/config.default.js
@@ -27,16 +27,6 @@ module.exports = appInfo => {
       ".html": "ejs"
     }
   };
-  config.sequelize = {
-    dialect: "mysql",
-    host: "106.15.203.69",
-    port: 3306,
-    database: "webgl",
-    username: "root",
-    password: "Jrh20060607+",
-    //underscored: true,  // 是否自动进行下划线转换（这里是因为DB默认的命名规则是下划线方式，而我们使用的大多数是驼峰方式）
-    timezone: '+08:00', // 时区，sequelize有很多自动时间的方法，都是和时区相关的，记得设置成东8区（+08:00）
-  };
   config.security = {
     csrf: {
       enable: false
@@ -94,6 +84,7 @@ module.exports = appInfo => {
 　　},
 　　  domainWhiteList: [ '*' ]
 　　};
+  // 跨域配置 
   config.cors = {
     origin: '*',
     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS'
